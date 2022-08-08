ad67c11285c8014d0ebf5c83517430a840200c92
Gabriel Lucchesi <lucchesi@lucchesi.local>
2022-08-07 17:00:55 -0700

    initializing client struct

--- client/client.go
+++ client/client.go
@@ -1,5 +1,63 @@
 package client
 
-func sum(a int32, b int32) (result int32) {
-	return a + b
+import (
+	"encoding/json"
+	"github.com/lucchesisp/pagarme-go/enums"
+	"github.com/lucchesisp/pagarme-go/utils"
+	"time"
+)
+
+type Client struct {
+	Name           string
+	Email          string
+	Code           string
+	DocumentNumber string
+	DocumentType   string
+	ClientType     string
+	Gender         string
+	Address        Address
+	Phones         []Phone
+	Birthday       time.Time
+	Metadata       string
+}
+
+type Address struct {
+	Country string
+	State   string
+	City    string
+	ZipCode string
+}
+
+type Phone struct {
+	HomePhone   PhoneFormat
+	MobilePhone PhoneFormat
+}
+
+type PhoneFormat struct {
+	CountryCode string
+	AreaCode    string
+	Number      string
+}
+
+func Create(payload Client) (response string, err error) {
+
+	payloadByte, payloadStringErr := json.Marshal(payload)
+
+	if payloadStringErr != nil {
+		return "", payloadStringErr
+	}
+
+	connection := utils.Connection{
+		Url:     enums.BASE_URL + "/customers",
+		Method:  "POST",
+		Payload: string(payloadByte),
+	}
+
+	response, responseErr := utils.SendRequest(connection)
+
+	if responseErr != nil {
+		return "", responseErr
+	}
+
+	return response, nil
 }
